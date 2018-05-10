e09057fe3f1f7cc0b8290813c7c4cc10d2ae6063
Aaron <aaronssy1@gmail.com>
2018-05-10 16:17:36 -0700

    created onclicks so enemy characters turn red

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+let marioHp = 100;
+let luigiHp = 100;
+let warioHp = 100;
+let bowserHp = 100;
+
+let marioAtk = 4;
+let luigiAtk = 4;
+let warioAtk = 6;
+let bowserAtk = 8;
+
+let marioCounter = 10;
+let luigiCounter = 10;
+let warioCounter = 15;
+let bowsercounter = 23;
+
+let isDefeatedEnemy = false;
+let isGameOver = false;
+
+function clickMario() {
+    $("#luigi-pic").css("background","red"); 
+    $("#wario-pic").css("background","red"); 
+    $("#bowser-pic").css("background","red"); 
+}
+
+function clickLuigi() {
+    $("#mario-pic").css("background","red"); 
+    $("#wario-pic").css("background","red"); 
+    $("#bowser-pic").css("background","red"); 
+}
+
+function clickWario() {
+    $("#luigi-pic").css("background","red"); 
+    $("#mario-pic").css("background","red"); 
+    $("#bowser-pic").css("background","red"); 
+}
+
+function clickBowser() {
+    $("#luigi-pic").css("background","red"); 
+    $("#wario-pic").css("background","red"); 
+    $("#mario-pic").css("background","red"); 
+}
+
+$("#charactercard1").on('click',function(){
+    clickMario();
+});
+
+$("#charactercard2").on('click',function(){
+    clickLuigi();
+});
+
+$("#charactercard3").on('click',function(){
+    clickWario();
+});
+
+$("#charactercard4").on('click',function(){
+    clickBowser();
+});
